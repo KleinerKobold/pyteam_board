8da0d5c6e903701309beea63719c782201ba713f
oliver <trollabeissa@gmail.com>
2022-08-02 20:06:33 +0200

    added observation

--- pyteam_board/team_member/models.py
+++ pyteam_board/team_member/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from tkinter import CASCADE
 from django.db import models
 from skills.models import Skill
+import datetime
 
 # Create your models here.
 class Person(models.Model):
@@ -18,4 +21,14 @@ class Experience(models.Model):
 
     def __str__(self):
         return f"Experience {self.name_text} of {self.person} with {self.level}"
+
+class Observation(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField('observed on',auto_now_add=True, blank=True)
+    persons = models.ManyToManyField(Person, through="Observed")
+    
+class Observed(models.Model):
+    person= models.ForeignKey(Person, on_delete=models.CASCADE)
+    observation = models.ForeignKey(Observation, on_delete=models.CASCADE)
+
 
